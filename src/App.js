7025aa073a9160c8bbd66d68b01823b6a89c21ad
Marius <marius@example.com>
2022-06-29 09:40:28 +0300

    env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
 function App() {
+  console.log('process.env.REACT_APP_BACKEND_URL', process.env.REACT_APP_BACKEND_URL);
+
   return (
     <div className='App'>
       <h1>Heloo again world</h1>
+      {/* /Login */}
     </div>
   );
 }
