534ce16795f1f926a582bfcf69a51a6ac4e92edf
Marius <marius@example.com>
2022-06-29 09:49:49 +0300

    router + routes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Steps
 
 1. .env reikalingas back end route http://localhost:5000
-2. setup react router dom @5
+2. setup react router dom @5 `npm i react-router-dom@^5`
 3. /login /register /posts /(homepage)
 4. 404 page
--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PostsPage from './pages/PostsPage';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  console.log('process.env.REACT_APP_BACKEND_URL', process.env.REACT_APP_BACKEND_URL);
+  // console.log('process.env.REACT_APP_BACKEND_URL', process.env.REACT_APP_BACKEND_URL);
 
   return (
     <div className='App'>
-      <h1>Heloo again world</h1>
-      {/* /Login */}
+      <Switch>
+        <Route path={'/register'}>
+          <RegisterPage />
+        </Route>
+        <Route path={'/login'}>
+          <LoginPage />
+        </Route>
+        <Route path={'/posts'}>
+          <PostsPage />
+        </Route>
+        <Route exact path={'/'}>
+          <HomePage />
+        </Route>
+        <Route path={'*'}>
+          <NotFoundPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
