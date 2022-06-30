53fef1f358d7dab3f2d796c36b206c7345bb1dac
Marius <marius@example.com>
2022-06-30 09:39:36 +0300

    history replace

--- src/store/authContext.js
+++ src/store/authContext.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { createContext } from 'react';
 
 const AuthContext = createContext({
@@ -6,25 +6,35 @@ const AuthContext = createContext({
   logout() {},
   isUserLoggedIn: '',
   token: null,
+  userEmail: '',
 });
 
 AuthContext.displayName = 'AuthContext';
 
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [userEmail, setUserEmail] = useState('');
 
-  const login = (gotToken) => {
-    console.log('gotToken ===', gotToken);
+  const login = (gotToken, gotEmail) => {
+    // console.log('gotToken ===', gotToken);
     setToken(gotToken);
+    setUserEmail(gotEmail);
   };
 
   const ctx = {
     login,
     logout() {},
-    isUserLoggedIn: !!null,
+    isUserLoggedIn: !!token,
     token,
+    userEmail,
   };
   return <AuthContext.Provider value={ctx}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
+
+// custom authCtx hook
+
+export const useAuthCtx = () => {
+  return useContext(AuthContext);
+};
