cacb426009cba4b7fde7d1c83c53a2283290411b
Marius <marius@example.com>
2022-06-30 10:51:34 +0300

    myFetchAuth, posts generated

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,11 +1,30 @@
 import Card from '../components/Card/Card';
+import { useState, useEffect } from 'react';
+import { baseUrl, myFetch, myFetchAuth } from '../utils';
+import { useAuthCtx } from '../store/authContext';
 
 function PostsPage() {
+  const { token } = useAuthCtx();
+  // console.log('token ===', token);
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const fetchResult = await myFetchAuth(`${baseUrl}/articles`, token);
+    console.log('fetchResult ===', fetchResult);
+    setPosts(fetchResult);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div className='container'>
       <h1 className='display-4 py-4'>Our Posts</h1>
       <div className='d-flex flex-wrap gap-1'>
-        <Card title='title' date='2017-01-01' content='very very impotant content' />
+        {posts.map((pObj) => (
+          <Card key={pObj.id} {...pObj} />
+        ))}
       </div>
     </div>
   );
